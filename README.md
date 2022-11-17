e08931d23d8384e05359a4b8a0ed4a9987d7b6de
Jose David Gomez <gomezjosedavid997@gmail.com>
2022-11-16 22:18:07 -0500

    MVC inplementado, Inicio de Desarrollo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# proyectoFinalHerramientas
+<h3 center> Proyecto Final Herramientas </h3>
+<hr>
+<br>
+<p>Proyecto de entrega final de la asignatura Herramientas de Programacion 3 aplicando lo aprendido en el desarrollo de paginas web utilizando HTML5, CSS3 y PHP, este proyecto es una tienda donde se reflejara un CRUD completo, adicional a esto incorporamos codigo en JavaScript, el backend esta programado en php con procedimieto almacenado</p>
+<br>
+<br>
+<hr>
+
+</p>
--- index.php
+++ index.php
@@ -1,31 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="./data/fenrir.ico">
-    <link rel="stylesheet" href="./css/index.css">
-    <title>Fenrir</title>
-</head>
-<body>
-    <nav>
-        <div class="secciones">
-            <a href="">Catálogo</a>
-            <a href="">Contacto</a>
-        </div>
-        <div class="logotipo">
-            <img src="./data/logotipo1.jpg">
-        </div>
-        <div class="perfil">
-            <img src="./data/login.png" alt="">
-            <a href="">Iniciar Sesión</a>
-            <img src="./data/carrito.png" alt="">
-            <a href="">Carrito De Compras</a>
-        </div>
-    </nav>
-    <main>
-        
-    </main>
-</body>
-</html>
+<?php
+    include './view/inicio.php'
+?>
