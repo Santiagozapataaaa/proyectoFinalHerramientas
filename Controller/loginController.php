a6267520beea396b89dcbef0944a2b8259d8c708
Santiago Franco Zapata <santiagofrancozapata5@gmail.com>
2022-11-18 15:50:06 -0500

    inicio de sesion malo

--- Controller/loginController.php
+++ Controller/loginController.php
@@ -2,9 +2,24 @@
     require_once("./model/loginModel.php");
     class loginController{
 
-        
-
         static function index(){
             include './view/login.php';
+        }
+
+        static function login(){
+            if(isset($_POST["enviar"]))
+            {
+                $usuario = $_POST["usuario"];
+                $password = $_POST["password"];
+                $consulta = new loginModelo();
+                $datos = [$usuario, $password];
+
+                $result = $consulta->consultar("tblusuarios", $datos);
+                if ($result)
+                {
+                    
+                    include ("./view/inicio.php");
+                }
+            }
         }
     }
