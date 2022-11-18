899065552e7c030dfd4adf04591611488cdf815c
Jose David Gomez <gomezjosedavid997@gmail.com>
2022-11-18 00:34:10 -0500

    menu desplegable carrito de compras

---
+++ view/js/app.js
@@ -0,0 +1,17 @@
+// Buscar
+let btnSearch = document.getElementById('search')
+let frmSearch = document.getElementById('frmsearch')
+
+btnSearch.addEventListener('click',()=>{
+    if(frmSearch.classList.contains("hidde"))
+    {
+        frmSearch.classList.add('show')
+        frmSearch.classList.remove('hidde')
+    }
+    else
+    {
+        frmSearch.classList.add('hidde')
+        frmSearch.classList.remove('show')
+    }
+    
+})
