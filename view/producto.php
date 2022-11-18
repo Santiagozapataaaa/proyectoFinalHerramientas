8d9d71559558390d332cf2e9cbf232794dc47f8b
Jose David Gomez <gomezjosedavid997@gmail.com>
2022-11-17 22:11:56 -0500

    correcciones en cista productos

--- view/producto.php
+++ view/producto.php
@@ -3,19 +3,24 @@
 include('./view/layouts/header.php');
 ?>
 
-    <form action="./producto.php" method="post" enctype="multipart/form-data" name="nuevo_producto" target="_blank">
-
-        <input class="form-control" type="text" placeholder="Nombre del producto" aria-label="default input example">
-        <input class="form-control" type="text" placeholder="Tipo del producto" aria-label="default input example">
-        <div class="descripcion">
-            <label for="exampleFormControlTextarea1" class="form-label">Descripcón del producto</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+<div class="container">
+    <div class="row">
+        <div class="col producto">
+            <form action="" method="post" enctype="multipart/form-data" target="_blank">
+                <input class="form-control" type="text" placeholder="Nombre del producto" aria-label="default input example">
+                <input class="form-control" type="text" placeholder="Tipo del producto" aria-label="default input example">
+                <div class="descripcion">
+                    <label for="exampleFormControlTextarea1" class="form-label">Descripcón del producto</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div class="archivo">
+                    <input type="file" name="archivosubido">
+                    <input type="submit" value="Enviar datos">
+                </div>
+            </form>
         </div>
-        <div class="archivo">
-            <input type="file" name="archivosubido">
-            <input type="submit" value="Enviar datos">
-        </div>
-    </form>
+    </div>
+</div>
 
 <?php
 //Footer
