52f664bb20d618d81883bf2d12ded40afa4658dc
Jose David Gomez <gomezjosedavid997@gmail.com>
2022-11-18 14:56:17 -0500

    estructura de la base de datos

---
+++ db/tblproductos.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 18-11-2022 a las 20:33:44
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dbo.fenrir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblproducto`
+--
+
+CREATE TABLE `tblproductos` (
+  `id` int(5) NOT NULL,
+  `strProducto` int(50) NOT NULL,
+  `strTipo` varchar(50) NOT NULL,
+  `strDescripcion` varchar(500) NOT NULL,
+  `intPrecio` int(10) NOT NULL,
+  `intCantidad` int(6) NOT NULL,
+  `strRuta` varchar(500) NOT NULL,
+  `datFecha_creacion` date NOT NULL DEFAULT current_timestamp(),
+  `datFecha_actualizacion` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tblproducto`
+--
+ALTER TABLE `tblproductos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tblproducto`
+--
+ALTER TABLE `tblproductos`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
