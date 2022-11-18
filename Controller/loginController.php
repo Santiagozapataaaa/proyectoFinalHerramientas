fe23a439409c79a8da9bd1553c0744d3fc3566b1
Santiago Franco Zapata <santiagofrancozapata5@gmail.com>
2022-11-18 11:17:15 -0500

    interfaz login terminada

--- Controller/loginController.php
+++ Controller/loginController.php
@@ -1,6 +1,9 @@
 <?php
-
+    require_once("./model/loginModel.php");
     class loginController{
+
+        
+
         static function index(){
             include './view/login.php';
         }
--- view/login.php
+++ view/login.php
@@ -5,26 +5,58 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./view/css/login.css">
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    <form class="formContainer">
-            <img class="imagenLogin" src="./view/img/Logotipo_fenrir_black.png" alt="logo">
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Usuario</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="User">
-    </div>
-    <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+    <h2>INICIO DE SESIÓN</h2>
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+            <form action="#">
+                <h1>Crear cuenta</h1>
+                <input type="text" placeholder="Nombre" />
+                <input type="number" placeholder="cedula" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button class="buttonIngresar">Registrarme</button>
+            </form>
+        </div>
+        <div class="form-container sign-in-container">
+            <form action="#">
+                <h1>Inicia Sesión</h1>
+                <input type="email" placeholder="Usuario" />
+                <input type="password" placeholder="Contraseña" />
+                <a href="#" class="forgot">Olvidaste tu contraseña?</a>
+                <button class="buttonIngresar">Ingresar</button>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Bienvenido de nuevo</h1>
+                    <p>Para mantenerte conectado con nosotros debes de loguearte con tus datos  personales</p>
+                    <button class="ghost" id="signIn">Ya tengo cuenta</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hola!!!</h1>
+                    <p>Aun no estas registrado? registrate dando click en el boton de aqui abajo</p>
+                    <button class="ghost" id="signUp">Registrarme</button>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <button type="submit" class="btn btn-dark">Continuar</button>
-    </form>
+    <script> 
+        const signUpButton = document.getElementById('signUp');
+        const signInButton = document.getElementById('signIn');
+        const container = document.getElementById('container');
 
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-</body>
+        signUpButton.addEventListener('click', () => {
+            container.classList.add("right-panel-active");
+        });
+
+        signInButton.addEventListener('click', () => {
+            container.classList.remove("right-panel-active");
+        });
+    </script>
+
 </html>
