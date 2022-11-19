e737ddba17f0cfe6448cdc195cd6152cded36070
Jose David Gomez <gomezjosedavid997@gmail.com>
2022-11-18 20:25:44 -0500

    correccion de rror en commit

--- model/registroModel.php
+++ model/registroModel.php
@@ -1,12 +1,35 @@
 <?php
-    class inicioModelo {
-        private $modelo;
-        private $conn;
+require_once("./model/registrarModel.php");
+class registroController
+{
+    static function index()
+    {
+        include './view/login.php';
+    }
 
-        public function __construct()
-        {
-            $this->Modelo = array();
-            include './controller/registroController.php';
-            $this->conn = $db;
+    static function registro()
+    {
+        if (isset($_POST['registrar'])) {
+            $nombre = htmlspecialchars($_POST['nombre']);
+            $apellido = htmlentities($_POST['apellido']);
+            $tipoDoc = htmlentities($_POST['tipoDoc']);
+            $doc = htmlentities($_POST['doc']);
+            $email = htmlentities($_POST['email']);
+            $rol = htmlentities(1);
+            $passwd = password_hash($_POST['passwd'], PASSWORD_DEFAULT, [15]);
+        }
+
+        $datos = [$nombre, $apellido, $tipoDoc, $doc, $email, $rol, $passwd];
+        $registro = new registroModelo();
+
+        $result = $registro->registrar("tblusuarios", $datos);
+
+        if ($result == true) {
+            header('location: ../iniciar_sesion');
+            die();
+        } else {
+            header('location: ../iniciar_sesion');
+            die();
         }
     }
+}
